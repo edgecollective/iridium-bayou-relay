7fa58d9468d84cb8fec82af167591ffe3b062b96
Don Blair <donblair@gmail.com>
2023-01-30 16:30:44 +0000

    got struct working

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const express = require('express');
+//const express = require('express');
+import express from 'express';
+
 const app = express();
-var request = require('request');
-var bodyParser = require('body-parser');
+
+
+//var request = require('request');
+import request from 'request';
+//var bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
 const port = 4000
 
 const imei = '300234065897350';
+import struct from './struct.mjs';
 
 app.get('/', (req, res) => {
 	  res.send('iridium relay!')
 });
+
+function toArrayBuffer(buf) {
+    const ab = new ArrayBuffer(buf.length);
+    const view = new Uint8Array(ab);
+    for (let i = 0; i < buf.length; ++i) {
+        view[i] = buf[i];
+    }
+    return ab;
+}
+
+let s = struct("<6f")
+let record = toArrayBuffer(Buffer.from("982f854090673f43bc35bb410028cf4188577d44646ccf41", "hex"));
+let results = s.unpack(record)
+console.log(results)
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
